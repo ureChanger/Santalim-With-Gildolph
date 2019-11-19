52d2c70c2b6368657344b8aa723a0ed969c07690
Sanglim00 <sanglim@kookmin.ac.kr>
2019-11-19 16:11:02 +0900

    backGround

--- main/display/SelectConversation.py
+++ main/display/SelectConversation.py
@@ -1,10 +1,11 @@
 import pygame
 
+# 파이게임 초기화
 pygame.init()
-
+# 윈도우 창의 크기 설정, 색상 설정
 screen = pygame.display.set_mode((640, 480))
 screen.fill((255, 255, 255))
-
+# 버튼을 만드는 클래스
 class makeButton():
     def __init__(self, color, x , y, width, height, text = ''):
         self.color = color
@@ -35,6 +36,9 @@ def redrawWindow():
     button1.draw(screen, (0, 0, 0))
     button2.draw(screen, (0, 0 ,0))
 
+def drawImage():
+    screen.blit(pygame.image.load("pooh.jpg"), (640, 480))
+
 run = True
 button1 = makeButton((0, 0, 255), 20, 265, 600, 90, "1) I'm hero, Let's go !!")
 button2 = makeButton((0, 0, 255), 20, 365 ,600, 90 , "2) I can't..Just run away..")
@@ -42,6 +46,8 @@ button2 = makeButton((0, 0, 255), 20, 365 ,600, 90 , "2) I can't..Just run away.
 while run :
     redrawWindow()
     pygame.display.update()
+    pygame.display.set_caption("Time Keeper Game !")
+    drawImage()
 
     for event in pygame.event.get():
         pos = pygame.mouse.get_pos()
