623f9a0b23796820b9f0b074d896fc44546b9094
Sanglim00 <sanglim@kookmin.ac.kr>
2019-12-15 01:37:05 +0900

    conversation5

--- main/display/Conversation.py
+++ main/display/Conversation.py
@@ -3,103 +3,91 @@ import os
 import main.item.ConversationItem
 import main.display.MakeButton
 
-# 파이게임 초기화
-pygame.init()
-# 윈도우 창의 크기 설정
-screen = pygame.display.set_mode((640, 480))
-
-makeButton = main.display.MakeButton.MakeButton
-convItem = main.item.ConversationItem.ConversationItem()
-
-os.chdir(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
-back = pygame.image.load("drawable/christmas1.jpg")
-screen.blit(pygame.transform.scale(back, (640, 480)), (0, 0))
-font = pygame.font.SysFont("comicsansms", 40)
-startText = font.render("Welcome to Santa village !", True, (0, 0, 0))
-screen.blit(startText, (135, 400))
-
 class GameStart:
 
     def __init__(self):
+        # 파이게임 초기화
         pygame.init()
-        self.button1 = makeButton((0, 108, 250), 20, 265, 600, 90, convItem.item[5])
-        self.button2 = makeButton((0, 108, 250), 20, 365, 600, 90, convItem.item[6])
+        # 윈도우 창의 크기 설정
         self.screen = pygame.display.set_mode((640, 480))
+        self.makeButton = main.display.MakeButton.MakeButton
+        self.convItem = main.item.ConversationItem.ConversationItem()
+
+        os.chdir(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+
+        self.back = pygame.image.load("drawable/snowback.png")
+        self.screen.blit(pygame.transform.scale(self.back, (640, 480)), (0, 0))
+        self.font = pygame.font.SysFont("comicsansms", 40)
+        self.startText = self.font.render("Welcome to Santa village !", True, (0, 0, 0))
+        self.screen.blit(self.startText, (35, 30))
+
+        self.button1 = self.makeButton((0, 108, 250), 20, 265, 600, 90, self.convItem.item[5])
+        self.button2 = self.makeButton((0, 108, 250), 20, 365, 600, 90, self.convItem.item[6])
         self.takeCheck = 1
         self.run = True
 
+    def redrawWindow(self):
+        self.button1.draw(self.screen, (0, 0, 0))
+        self.button2.draw(self.screen, (0, 0, 0))
 
     # 행복한 산타마을 등장
     def Take1(self):
-        christmasPresent = pygame.image.load("drawable/christmas3.jpg")
-        screen.blit(pygame.transform.scale(christmasPresent, (640, 480)), (0, 0))
-        # santa = pygame.image.load("drawable/santa-claus.png")
-        # santa = pygame.transform.scale(santa, (180, 180))
-        # screen.blit(santa, (420, 240))
-        self.takeCheck += 1
+        self.christmasPresent = pygame.image.load("drawable/christmas3.jpg")
+        self.screen.blit(pygame.transform.scale(self.christmasPresent, (640, 480)), (0, 0))
 
     # 누군가 선물을 다 가져간다고 한다 !
     def Take2(self):
         font = pygame.font.SysFont("comicsansms", 40)
-        screen.fill((0, 0, 0))
-        # pygame.draw.rect(screen, (255, 255, 255), [50, 360, 530, 80], 3)
-        text = font.render(convItem.item[2], True, ((255, 0, 0)))
-        screen.blit(text, (150, 370))
-        self.takeCheck += 1
+        self.screen.fill((200, 200, 200))
+        self.robbrr = pygame.image.load("drawable/robber.png")
+        self.screen.blit(self.robbrr,(240, 250))
+        self.text = font.render(self.convItem.item[2], True, ((255, 0, 0)))
+        self.screen.blit(self.text, (80, 370))
 
     # 도둑이다 !
     def Take3(self):
-        screen.fill((0, 0, 0))
-        badbad = pygame.image.load("drawable/devil.png")
-        badbad = pygame.transform.scale(badbad, (230, 230))
-        screen.blit(badbad, (200, 100))
-        self.takeCheck += 1
+        self.back = pygame.image.load("drawable/snowback2.png.jpg")
+        self.screen.blit(pygame.transform.scale(self.back, (640, 480)), (0, 0))
+        self.badbad = pygame.image.load("drawable/criminal.png")
+        self.badbad = pygame.transform.scale(self.badbad, (100, 100))
+        self.screen.blit(self.badbad, (100, 170))
 
     # 산타집을 턴 도둑, 눈 앞에서 선물 뺏긴 산타
     def Take4(self):
-        snowWorld = pygame.image.load("drawable/snowworld.PNG")
-        screen.blit(pygame.transform.scale(snowWorld, (640, 480)), (0, 0))
-        theif = pygame.image.load('drawable/criminal.png')
-        santaclaus = pygame.image.load('drawable/santaclaus.png')
-        # 위에 도둑도망가기 , 산타는 따라가다 멈추기
-        self.takeCheck += 1
+        self.beggarSanta = pygame.image.load("drawable/beggarSanta.PNG")
+        self.screen.blit(pygame.transform.scale(self.beggarSanta, (640, 480)), (0, 0))
 
     # 산타 슬퍼 누가 내 선물 찾아와
     def Take5(self):
         crysanta = pygame.image.load('drawable/crysanta.PNG')
-        screen.blit(pygame.transform.scale(crysanta, (640, 480)), (0, 0))
+        self.screen.blit(pygame.transform.scale(crysanta, (640, 480)), (0, 0))
         font = pygame.font.SysFont("comicsansms", 30)
-        text2 = font.render(convItem.item[3], True, ((0, 0, 0)))
-        screen.blit(text2, (100, 390))
-        self.takeCheck += 1
+        text2 = font.render(self.convItem.item[3], True, ((0, 0, 0)))
+        self.screen.blit(text2, (100, 390))
 
     # (유저네임)동글이가 간다
     def Take6(self):
         font = pygame.font.SysFont("comicsansms", 30)
-        circleBack = pygame.image.load("drawable/circleBack.PNG")
-        screen.blit(pygame.transform.scale(circleBack, (640, 480)), (0, 0))
-        text3 = font.render(convItem.item[4], True, (0, 0, 0))
-        screen.blit(text3, (100, 400))
-        self.takeCheck += 1
+        circleBack = pygame.image.load("drawable/donggeul.PNG")
+        self.screen.blit(pygame.transform.scale(circleBack, (640, 480)), (0, 0))
+        text3 = font.render(self.convItem.item[4], True, (0, 0, 0))
+        self.screen.blit(text3, (110, 40))
 
     def Take7(self):
-        screen.fill((13, 132, 0))
-        self.button1.draw(screen, (0, 0, 0))
-        self.button2.draw(screen, (0, 0, 0))
-        self.takeCheck += 1
+        self.screen.fill((13, 132, 0))
+        self.redrawWindow()
 
     # 게임 설명 넣기
-    def Take8_1(self):
-        screen.fill((0, 0, 0))
+    def Take8(self):
+        self.screen.fill((0, 0, 0))
+        font = pygame.font.SysFont("comicsansms", 40)
+        self.text4 = font.render(self.convItem.item[5], True, ((255, 255, 255)))
+        self.screen.blit(self.text4, (150, 370))
 
-    # 게임 연결하기
-    def Take8_2(self):
-        screen.fill(255, 255, 255)
-
-    def run(self):
+    def Run(self):
         while self.run:
             pygame.display.update()
-            pygame.display.set_caption("Time Keeper Game !")
+            pygame.display.set_caption("SNOW RUN")
 
             for event in pygame.event.get():
                 pos = pygame.mouse.get_pos()
@@ -132,26 +120,22 @@ class GameStart:
                     elif self.takeCheck == 7:
                         self.Take7()
 
-                if self.takeCheck == 7:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if self.button1.isOver(pos):
-                            self.Take8_1()
-                            print("button1 was clicked !")
-                        elif self.button2.isOver(pos):
-                            self.Take8_2()
-                            print("button2 was clicked !")
+                # 버튼 눌렸을 때
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.button1.isOver(pos):
+                        self.Take8()
+                        print("button1 was clicked !")
+                    elif self.button2.isOver(pos):
+                        import main.gilhyeon.GameTest
+                        print("button2 was clicked !")
 
-                    if event.type == pygame.MOUSEMOTION:
-                        if self.button1.isOver(pos):
-                            self.button1.color = (255, 255, 0)
-                        else:
-                            self.button1.color = (172, 164, 255)
+                if event.type == pygame.MOUSEMOTION:
+                    if self.button1.isOver(pos):
+                        self.button1.color = (255, 255, 0)
+                    else:
+                        self.button1.color = (172, 164, 255)
 
-                        if self.button2.isOver(pos):
-                            self.button2.color = (250, 250, 0)
-                        else:
-                            self.button2.color = (172, 164, 255)
-
-if __name__ == '__main__':
-    gameDisplay = GameStart()
-    gameDisplay.run
+                    if self.button2.isOver(pos):
+                        self.button2.color = (250, 250, 0)
+                    else:
+                        self.button2.color = (172, 164, 255)
